016c3f1073b1f7ea04434c61a962ba6cd3c45a29
Naoki Yoshizawa <naoki.4438@gmail.com>
2020-02-05 03:56:07 +0900

    独立挙動モード追加(add)
    jtalkにて当プログラム単体での文章の読み上げに対応

--- iCAN2020Python/ReadingTextUnit.py
+++ iCAN2020Python/ReadingTextUnit.py
@@ -1,3 +1,27 @@
-import jtalk
+import subprocess
+import winsound
 
-jtalk.jtalk(u'日本語を話します') 
+def jtalk(t):
+    # depend on your install folder
+    OPENJTALK_BINPATH = 'c:/open_jtalk/bin'
+    OPENJTALK_DICPATH = 'c:/open_jtalk/bin/dic'
+    OPENJTALK_VOICEPATH = 'c:/open_jtalk/bin/nitech_jp_atr503_m001.htsvoice'
+    open_jtalk=[OPENJTALK_BINPATH + '/open_jtalk.exe']
+    mech=['-x',OPENJTALK_DICPATH]
+    htsvoice=['-m',OPENJTALK_VOICEPATH]
+    speed=['-r','1.0']
+    outwav=['-ow','open_jtalk.wav']
+    cmd=open_jtalk+mech+htsvoice+speed+outwav
+    c = subprocess.Popen(cmd,stdin=subprocess.PIPE)
+
+    # convert text encoding from utf-8 to shitf-jis
+    c.stdin.write(t.encode('shift-jis'))
+    c.stdin.close()
+    c.wait()
+
+    # play wav audio file with winsound module
+    winsound.PlaySound('open_jtalk.wav', winsound.SND_FILENAME)
+
+if __name__ == '__main__':
+    print("独立挙動モード")
+    jtalk(u'ユニットとしては動いてません、独立挙動モードです')
